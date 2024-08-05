dcacec6b3498859acce214a3230dda1063b87d7e
Niharika4545 <91818943+Niharika4545@users.noreply.github.com>
2024-08-05 13:40:48 +0300

    Update firebase.jsx

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-	apiKey: "",
-	authDomain: "",
-	projectId: "",
-	storageBucket: "",
-	messagingSenderId: "",
-	appId: "",
-	measurementId: ""
+	apiKey: "AIzaSyAxvyYN55feKaIx5u2fES-cjqwYlp7P_cg",
+	  authDomain: "instagram-572da.firebaseapp.com",
+	  projectId: "instagram-572da",
+	  storageBucket: "instagram-572da.appspot.com",
+	  messagingSenderId: "557146395658",
+	  appId: "1:557146395658:web:fa3c75f331f2d78767bbf3",
+	  measurementId: "G-K3W4D5BHTP"
   };
 
 const app = initializeApp(firebaseConfig);
